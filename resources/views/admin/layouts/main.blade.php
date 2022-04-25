311bf8bdd22ac5fcd73c8dd186af2631651f22c8
specialsnipe@mail.ru <M1J2$Ho%E>
2022-04-25 15:55:08 +0500

    review

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Theme style -->
     <link rel="stylesheet" href="{{asset('public/dist/css/adminlte.min.css')}}">
-    <link rel="stylesheet" href="{{asset('public/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
+    <link rel="stylesheet" href="{{asset('public//plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
     <!-- overlayScrollbars -->
     <link rel="stylesheet" href="{{asset('public/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
     <!-- Daterange picker -->
@@ -27,7 +27,7 @@
 
     <!-- Preloader -->
     <div class="preloader flex-column justify-content-center align-items-center">
-        <img class="animation__shake" src="{{asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTELogo" height="60" width="60">
+        <img class="animation__shake" src="{{asset('public/dist/img/AdminLTELogo.png')}}" alt="AdminLTELogo" height="60" width="60">
     </div>
 
     <!-- Navbar -->
@@ -69,28 +69,30 @@
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script src="{{asset('public/public/plugins/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('public/plugins/jquery/jquery.min.js')}}"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="{{asset('public/public/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
-<script src="{{asset('public/public/plugins/select2/js/select2.full.min.js')}}"></script>
+<script src="{{asset('public/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+<script src="{{asset('public/plugins/select2/js/select2.full.min.js')}}"></script>
 
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
     $.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- daterangepicker -->
-<script src="{{asset('public/public/plugins/moment/moment.min.js')}}"></script>
-<script src="{{asset('public/public/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
-<script src="{{asset('public/public/plugins/daterangepicker/daterangepicker.js')}}"></script>
+<script src="{{asset('public/plugins/moment/moment.min.js')}}"></script>
+<script src="{{asset('public/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
+<script src="{{asset('public/plugins/daterangepicker/daterangepicker.js')}}"></script>
 <!-- Tempusdominus Bootstrap 4 -->
 <!-- overlayScrollbars -->
-<script src="{{asset('public/public/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
+<script src="{{asset('public/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
 <!-- AdminLTE App -->
-<script src="{{asset('public/public/dist/js/adminlte.js')}}"></script>
+<script src="{{asset('public/dist/js/adminlte.js')}}"></script>
 <!-- AdminLTE for demo purposes -->
 <script>
     $('.select2').select2()
 </script>
 </body>
 </html>
+
+
 
